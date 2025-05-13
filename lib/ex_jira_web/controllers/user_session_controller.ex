e4441807062e15a3831184abebccc13ef6c11fb4
Kevin <kevinam99@gmail.com>
2025-05-14 01:41:15 +0530

    Remove unused plug

--- lib/ex_jira_web/controllers/user_session_controller.ex
+++ lib/ex_jira_web/controllers/user_session_controller.ex
@@ -19,18 +19,27 @@ defmodule ExJiraWeb.UserSessionController do
   end
 
   defp create(conn, %{"user" => user_params}, info) do
-    %{"email" => email, "password" => password} = user_params
+    %{"email" => email, "password" => password, "organisation_id" => org_id} = user_params
 
-    if user = Accounts.get_user_by_email_and_password(email, password) do
+    with %ExJira.Accounts.User{} = user <-
+           Accounts.get_user_by_email_and_password(email, password),
+         %ExJira.Accounts.AccessControl{} <-
+           Accounts.get_access_control_by(%{user_id: user.id, organisation_id: org_id}) do
       conn
       |> put_flash(:info, info)
       |> UserAuth.log_in_user(user, user_params)
     else
-      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
-      conn
-      |> put_flash(:error, "Invalid email or password")
-      |> put_flash(:email, String.slice(email, 0, 160))
-      |> redirect(to: ~p"/users/log_in")
+      nil ->
+        # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
+        conn
+        |> put_flash(:error, "Invalid email or password or organisation ID.")
+        |> put_flash(:email, String.slice(email, 0, 160))
+        |> redirect(to: ~p"/users/log_in")
+
+      _ ->
+        conn
+        |> put_flash(:error, "You are not authorized to log in.")
+        |> redirect(to: ~p"/users/log_in")
     end
   end
 
